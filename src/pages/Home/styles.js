f28e661de0a3aad6551237ca0217b9c23ffea5b5
Luis Fernando <luisfernandobega@gmail.com>
2023-09-06 13:47:46 -0300

    Home footer adjust added

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,11 +2,16 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
 
   overflow: auto;
 
   main {
+    flex-grow: 1;
+
     > div:nth-child(2) {
       margin: 0 0 2.4rem 2.4rem;
     }
